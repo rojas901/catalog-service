172fc3fc6bddf6fd6a1094ee5a5f712fd39877a2
Carlos Rojas <caranroja@outlook.com>
2024-04-24 21:31:13 -0500

    Se agrega bookDataLoader y configuracion para que solo funcione en test environment

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,14 +1,23 @@
 package com.polarbookshop.catalogservice;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.polarbookshop.catalogservice.config.PolarProperties;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HomeController {
 
+	private final PolarProperties polarProperties;	
+
+	public HomeController(PolarProperties polarProperties) {
+		this.polarProperties = polarProperties;
+	}
+
 	@GetMapping("/")
 	public String getGreeting() {
-		return "Welcome to the book catalog!";
+		return polarProperties.getGreeting();
 	}
 
 }
